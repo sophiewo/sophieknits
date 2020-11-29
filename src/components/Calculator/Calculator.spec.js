8c1b0cb9869d88d21ab738721c3756e96e9570fd
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-11-29 13:14:57 +0000

    Tests passing

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -5,7 +5,7 @@ import Calculator from './Calculator'
 describe("Calculator", () => {
   test("Shows Calculator", () => {
     render(<Calculator />);
-    const text = screen.getByText("Calculator");
+    const text = screen.getByText("Stitch Decrease Calculator");
     expect(text).toBeInTheDocument();
   });
 });
