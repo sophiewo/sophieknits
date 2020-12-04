2738d5884ce6f05f9e4bc235ccdeed390ce4acca
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-12-04 11:30:16 +0000

    Builds out component structure for header and nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../sophieknits-hm.png'
+
+const navBar = () => {
+
+  return (
+    <header>
+      <nav class="pa3 pa4-ns">
+
+        <a
+          class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns"
+          href="#"
+          title="Home">
+          <img src={logo} center alt='SophieKnits_' />
+        </a>
+        <div class="tc pb3">
+          <a class="link dim black f1 f3-ns dib mr3" href="#" title="Home">TOOLS</a>
+          <a class="link dim black f1 f3-ns dib mr3" href="#" title="Instagram">INSTAGRAM</a>
+        </div>
+      </nav>
+    </header>
+  )
+};
+
+export default navBar;
