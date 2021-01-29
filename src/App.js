8fdc6a8814c6c00c1c4d5155fafb701c9de88987
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2021-01-29 23:28:34 +0000

    starts setting out grid elements

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Header from './components/Header/Header';
 import Card from './components/Card/Card';
+import HomePage from './components/Homepage/HomePage'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <HomePage />
       <Card title="Decrease Calculator" />
     </div>
 
--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,47 +1,71 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+
+import logo from '../../sophieknits-hm.png';
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
+  }
+
 }));
 
-export default function CenteredGrid() {
+export default function HomePage() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=12</Paper>
+      <Grid container xs={12} spacing={1}>
+        <Grid item xs>
+          <a
+            href="/"
+            title="Home">
+            <img src={logo} center alt='SophieKnits_' />
+          </a>
         </Grid>
+
+            <Grid item xs={12}>
+              <a
+                href="/"
+                title="Home">
+                <img src={logo} center alt='SophieKnits_' />
+              </a>
+            </Grid>
+
         <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
+          <a
+            href="/"
+            title="Home">
+            <img src={logo} center alt='SophieKnits_' />
+          </a>
         </Grid>
-        <Grid item xs={6}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-        <Grid item xs={3}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
-      </Grid>
-    </div>
+
+            </Grid>
+            </div>
+
+
+
+    //     <Grid item xs={6}>
+    //       <Paper className={classes.paper}>xs=6</Paper>
+    //     </Grid>
+    //     <Grid item xs={6}>
+    //       <Paper className={classes.paper}>xs=6</Paper>
+    //     </Grid>
+    //     <Grid item xs={3}>
+    //       <Paper className={classes.paper}>xs=3</Paper>
+    //     </Grid>
+    //     <Grid item xs={3}>
+    //       <Paper className={classes.paper}>xs=3</Paper>
+    //     </Grid>
+    //     <Grid item xs={3}>
+    //       <Paper className={classes.paper}>xs=3</Paper>
+    //     </Grid>
+    //     <Grid item xs={3}>
+    //       <Paper className={classes.paper}>xs=3</Paper>
+    //     </Grid>
+    //   </Grid>
+    // </div>
   );
 }
