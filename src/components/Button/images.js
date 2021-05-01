5c6539ab4bd6457f42703e41897a01990d160cd0
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2021-05-01 09:51:45 +0100

    adds id as constant

--- src/components/Button/images.js
+++ src/components/Button/images.js
@@ -4,19 +4,22 @@ const images = [
       id: 1,
       source: 'img/sk_1.jpeg',
       title: 'deBrosse',
-      width: '100%'
+      width: '100%',
+      height: '100%'
     },
     {
       id: 2,
       source: 'img/sk_2.jpeg',
       title: 'Petite Knit',
-      width: '100%'
+      width: '100%',
+      height: '100%'
     },
     {
       id: 3,
       source: 'img/sk_3.jpeg',
       title: 'Purl Soho Blanket',
-      width: '100%'
+      width: '100%',
+      height: '100%'
   },
   {
     id: 4,
@@ -28,13 +31,15 @@ const images = [
     id: 5,
     source: 'img/sk_4.jpg',
     title: 'Gregoria Fibers',
-    width: '100%'
+    width: '100%',
+    height: '100%'
   },
   {
     id: 6,
     source: 'img/sk_6.jpeg',
     title: 'Gregoria Fibers',
-    width: '100%'
+    width: '100%',
+    height: '100%'
   }
   ]
 
--- src/components/Cloudinary/CloudinaryImages.js
+++ src/components/Cloudinary/CloudinaryImages.js
@@ -4,10 +4,12 @@ import React from 'react';
 export default CloudinaryImages 
 function CloudinaryImages() {
 
+  const id ="v1619087662/HomePage/"
+
   return (
   
   <CloudinaryContext cloudName="sophieknits">
-    <Image publicId="v1619087662/HomePage/sk_5.jpg" width="100%" />
+    <Image publicId={id + "sk_3.jpg"} width="100%" />
   </CloudinaryContext>
   
   )}
